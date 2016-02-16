11c265db03ab9ec4d31563109c1f0565ab4fc915
d_velqnov <d_velqnov@abv.bg>
2016-02-16 20:32:03 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hallo World!");
+		System.out.println("Hallo Dimitar!");
+		System.out.println("How are you World?");
+		System.out.println("I'm good, thank you. And how are you?");
+		System.out.println("I'm fine too. ");
+	}
+
+}
